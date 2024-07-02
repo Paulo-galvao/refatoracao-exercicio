7e25ca3fad6126560de77c07cd20d01badfe85e6
Paulo-galvao <paulogalvaoj21@gmail.com>
2024-07-02 19:18:53 -0300

    mostrar quantidade de aprovados e reprovados

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 let notas = [7.5, 8.0, 6.0, 9.5, 5.0, 8.5, 7.0, 10.0, 6.5, 9.0];
 
-let soma = 0;
-let maiorNota = notas[0]; 
-let menorNota = notas[0];
-
 function mostrarMaiorNota(listaDeNotas) {
+    let maiorNota = listaDeNotas[0];
     for (let i = 0; i < listaDeNotas.length; i++) {
         if (listaDeNotas[i] > maiorNota) {
             maiorNota = listaDeNotas[i];
@@ -14,6 +11,7 @@ function mostrarMaiorNota(listaDeNotas) {
 }
 
 function mostrarMenorNota(listaDeNotas) {
+    let menorNota = listaDeNotas[0];
     for (let i = 0; i < listaDeNotas.length; i++) {
         if(listaDeNotas[i] < menorNota) {
             menorNota = listaDeNotas[i];
@@ -35,7 +33,7 @@ function mostrarAprovados(listaDeNotas) {
     
 }
 
-function mostarReprovados(listaDeNotas) {
+function mostrarReprovados(listaDeNotas) {
     const lista = [];
     for (let i = 0; i < listaDeNotas.length; i++) {
         if(notas[i] < 6) {
@@ -47,7 +45,25 @@ function mostarReprovados(listaDeNotas) {
     return lista;
 }
 
+function contarAprovados(listaDeNotas) {
+    let aprovados = 0;
+    mostrarAprovados(listaDeNotas).forEach(aluno => {
+        aprovados++;
+    }); 
+    return aprovados;
+}
+
+function contarReprovados(listaDeNotas) {
+    let reprovados = 0;
+    mostrarReprovados(listaDeNotas).forEach(aluno => {
+        reprovados++;
+    }); 
+    return reprovados;
+}
+
 console.log(`Maior nota: ${mostrarMaiorNota(notas)}`);
 console.log(`Menor nota: ${mostrarMenorNota(notas)}`);
 console.log(`Notas acima da média: ${mostrarAprovados(notas)}`);
-console.log(`Notas abaixo da média: ${mostarReprovados(notas)}`);
+console.log(`Total de aprovados: ${contarAprovados(notas)}`);
+console.log(`Notas abaixo da média: ${mostrarReprovados(notas)}`);
+console.log(`Total de reprovados: ${contarReprovados(notas)}`);
